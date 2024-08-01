5ccef2fe6ce642df3044718aa720f067bc345a6f
Gn842 <natoguadalupe9@gmail.com>
2024-08-01 18:05:21 -0400

    feat: Add multiple question and answer inputs with validation

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 # var = Input
-question = input("Ask a question: ")
+question1 = input("Ask a question: ")
 answer = input("Give me the answer: ")
-
 my_answer = input(" " + question)
+
+question2 = input("Ask a another question: ")
+answer2 = input("Give me the answer: ")
+my_answer2 = input(" " + question1)
+
+question3 = input("Ask one more question: ")
+answer3 = input("Give me the answer: ")
+my_answer3 = input("" + question)
+
 if my_answer == answer:
     print("Thats correct")
 else:
     print("Thats wrong")
+
+
+
+if my_answer2 == answer2:
+    print("Correct!")
+else:
+    print("Incorrect!")
+
 # ask for answer for every Question
 
 # Save the questions
