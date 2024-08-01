746a3ef2eb81028a589332723eb9f9bf9699202f
Gn842 <natoguadalupe9@gmail.com>
2024-08-01 17:56:47 -0400

    Test input

---
+++ main.py
@@ -0,0 +1,16 @@
+# var = Input
+question = input("Ask a question: ")
+answer = input("Give me the answer: ")
+
+my_answer = input(" " + question)
+if my_answer == answer:
+    print("Thats correct")
+else:
+    print("Thats wrong")
+# ask for answer for every Question
+
+# Save the questions
+
+# test mode
+
+# Submit for grade
