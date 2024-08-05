5c81fbc700a59e25b2b8e9a0e0d8e05db26f09ce
Gn842 <natoguadalupe9@gmail.com>
2024-08-04 21:25:57 -0400

    feat: implement interactive Q&A system with scoring
    - Added a loop to collect questions and answers from the user.
    - Stored questions and answers in a hashmap.
    - Implemented logic to quiz the user based on the collected questions.
    - Calculated and printed the final score with proper rounding.

--- main.py
+++ main.py
@@ -1,53 +1,44 @@
-# # var = Input
-# question1 = input("Ask a question: ")
-# answer = input("Give me the answer: ")
-# my_answer = input(" " + question1)
-
-# while answer != my_answer:
-#     print("Try again!")
-# else:
-#     print("That's correct!")
-
-# question2 = input("Ask a another question: ")
-# answer2 = input("Give me the answer: ")
-# my_answer2 = input(" " + question2)
-
-# while answer2 != my_answer2:
-#     print("Try again")
-# else:
-#     print("That correct!")
-
-# question3 = input("Ask one more question: ")
-# answer3 = input("Give me the answer: ")
-# my_answer3 = input("" + question3)
-
-# while answer3 != my_answer3:
-#     print("Try again!")
-# else:
-#     print("That's correct!")
-
-# if my_answer3 == answer3:
-#     print("Correct!")
-# else:
-#     print("Incorrect!")
-# ask for answer for every Question
-
-# Save the questions
-
-# test mode
-
-# Submit for grade
-
-
+import math
 
 another = True
 
+hashmap = {} # '2+2': '4', '1+1': 2
+
+# Add all Questions and answers
 while another:
-    question1 = input("Ask a question: ")
-    answer = input("Give me the answer:") 
-    myanswer = input ("\n")
+    question = input("Ask a question: ")
+    answer = input("Give me the answer: ") 
+
+    hashmap[question] = answer
+
     again = input("Do you want to add another question? respond y or n: ")
     if again == 'y':
         another = True
     if again == 'n':
         another = False
+
+points = 100
+wrongAmount = 0
+
+# Ask every question in the hashmap
+# check if the question is right
+for q in hashmap:
+    myAnswer = input(q)
+    if hashmap[q] == myAnswer:
+        print('correct')
+    else:
+        print("wrong")
+        wrongAmount += 1
+
+
+# (2-1) /2
+score = (len(hashmap) - wrongAmount) / len(hashmap) * 100
+print(math.ceil(score))
+
+
+ 
+
+      
+        
+
+        
